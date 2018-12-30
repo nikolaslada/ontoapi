50c06c230a66e7d156c89649db6824d5d385d4dc
Nikolas Lada <nikolas.lada@gmail.com>
2018-12-30 19:02:44 +0100

    Added UserTransformer class

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+final class UserTransformer extends TransformerAbstract
+{
+    
+    /** @var bool */
+    protected $showEmail = true;
+    
+    
+	public function transform(User $user): array 
+	{
+		$response = [
+			'id'   => $user->id,
+			'name' => $user->name
+		];
+        
+		if ($this->showEmail === true) {
+			$response['email'] = $user->email;
+		}
+        
+		return $response;
+	}
+    
+    
+	public function hideEmail(): UserTransformer
+	{
+		$this->showEmail = false;
+		return $this;
+	}
+    
+}
